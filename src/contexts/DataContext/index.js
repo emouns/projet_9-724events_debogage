44f0b7eace21f5889b99b38c2018d42cd8a4c904
moun <mounirfendou@gmail.com>
2026-06-10 12:09:02 +0200

    modif com

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -29,7 +29,7 @@ export const DataProvider = ({ children }) => {
   useEffect(() => {    
     if (data) return;
     getData();
-  }, [getData]);        // getData ce lancera une fois stabiliser par useCallback //
+  }, [getData]);        // getData ce lancera une fois il est stabiliser par useCallback //
   
   return (
     <DataContext.Provider
